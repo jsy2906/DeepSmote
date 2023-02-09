6a7d29c2a72cdb149d1757553a5655ef3c25f618
조세영 | Jo Seyeong <69945030+jsy2906@users.noreply.github.com>
2023-02-10 03:09:09 +0900

    add cfg dict

--- data_load.py
+++ data_load.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 
 
+cfg = {
+    'train_bs': 25,
+    'valid_bs': 25,
+    }
+
 
 def get_img(path, sub_path=None):
     im_bgr = cv2.imread(path)
@@ -12,7 +17,6 @@ def get_img(path, sub_path=None):
 class AEDataset(Dataset):
     def __init__(self, df, data_root, 
                  transforms=None, 
-                 noise = False, prob=0.15,
                  output_label=True, 
                  one_hot_label=False):
         
@@ -20,15 +24,12 @@ class AEDataset(Dataset):
         self.df = df.reset_index(drop=True).copy()
         self.data_root = data_root
         self.transforms = transforms
-        self.noise = noise
-        self.prob = prob
         
         self.output_label = output_label
         self.one_hot_label = one_hot_label
         
         if output_label == True:
             self.labels = self.df['label'].values
-            #print(self.labels)
             
             if one_hot_label is True:
                 self.labels = np.eye(self.df['label'].max()+1)[self.labels]
@@ -37,23 +38,6 @@ class AEDataset(Dataset):
     def __len__(self):
         return self.df.shape[0]
     
-    def plus_noise(self, image):
-        output = image.copy()
-        if len(image.shape) == 2:
-            black = 0
-            white = 255            
-        else:
-            colorspace = image.shape[2]
-            if colorspace == 3:  # RGB
-                black = np.array([0, 0, 0], dtype='uint8')
-                white = np.array([255, 255, 255], dtype='uint8')
-            else:  # RGBA
-                black = np.array([0, 0, 0, 255], dtype='uint8')
-                white = np.array([255, 255, 255, 255], dtype='uint8')
-        probs = np.random.random(image.shape[:2])
-        output[probs < (self.prob / 2)] = black
-        output[probs > 1 - (self.prob / 2)] = white
-        return output
     
     def __getitem__(self, index: int):
         
@@ -65,20 +49,12 @@ class AEDataset(Dataset):
         
         
         if self.transforms:
-            if self.noise:
-                noise_img = self.plus_noise(img)
-                noise_img = self.transforms(image=noise_img)
-                img = self.transforms(image=img)['image']
-            else:
-                img = self.transforms(image=img)['image']
+            img = self.transforms(image=img)['image']
         
                             
         # do label smoothing
         if self.output_label == True:
-            if self.noise:
-                return img, noise_img, target
-            else:
-                return img, target
+            return img, target
         else:
             return img
           
@@ -120,7 +96,7 @@ def prepare_dataloader(image_size, train, valid, trn_batch=cfg['train_bs'], val_
     
     from catalyst.data.sampler import BalanceClassSampler
 
-    train_ds = AEDataset(train, trn_root, transforms=get_train_transforms(image_size), noise=False, output_label=True, )
+    train_ds = AEDataset(train, trn_root, transforms=get_train_transforms(image_size), output_label=True, one_hot_label=False)
     valid_ds = AEDataset(valid, val_root, transforms=get_valid_transforms(image_size), output_label=True)
     
     train_loader = torch.utils.data.DataLoader(
